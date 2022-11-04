45f846536f514140e74a7b0443dabf94fa460b40
横山 <96154391+YokoyamaYuta@users.noreply.github.com>
2022-11-04 23:05:07 +0900

    二人対戦　完成間近

--- Assets/Users/Yuta/Script/Test_Player1.cs
+++ Assets/Users/Yuta/Script/Test_Player1.cs
@@ -4,15 +4,137 @@ using UnityEngine;
 
 public class Test_Player1 : MonoBehaviour
 {
+    //マウス操作
+
+    Rigidbody2D rigid2D;
+    Vector2 startPos;
+    [SerializeField] private GameObject attackPrefab;
+    [SerializeField] private GameObject comaObject;
+
+    public float speed;
+    public static float start_time = 0f;
+    public static float time = 0f;
+    private float Action_time = 1f;
+
+    public static bool start = true;
+    public static bool Action = false;
+    public static bool Avoidance = false;
+    private bool mouseDrag = false;
+    public static bool Ready_P1 = false;
+    public static bool Start_Time_P1 = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.rigid2D = comaObject.GetComponent<Rigidbody2D>();
+        start = true;
+        Action = false;
+        Avoidance = false;
+        Ready_P1 = false;
+
+        start_time = 0f;
+        time = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (start)
+        {
+            if (Start_Time_P1 && Test_Player2.Start_Time_P2)
+            {
+                Debug.Log("レディー");
+                start_time += Time.deltaTime;
+            }
+
+            //マウスの動きと反対方向に発射される
+            if (InputProvider.Instance.GetFire1Down())
+            {
+                //マウスを押したポジションを代入
+                this.startPos = Input.mousePosition;
+
+                Start_Time_P1 = true;
+            }
+            else if (start_time >= 3f)
+            {
+                //マウスを離したポジションを代入
+                Vector2 endPos = Input.mousePosition;
+                //マウスの動きと反対方向に移動
+                KomaMove(endPos);
+
+                start = false;
+                Action = true;
+                Avoidance = true;
+            }
+        }
+        else
+        {
+            time += Time.deltaTime;
+            Ready_P1 = false;
+            Start_Time_P1 = false;
+        }
+
+        //攻撃＆回避
+        if (Action_time <= time)
+        {
+            //マウスドラッグ開始
+            if (InputProvider.Instance.GetFire1Down())
+            {
+                //マウスのドラッグ開始ポジションを代入
+                this.startPos = Input.mousePosition;
+                mouseDrag = true;
+            }
+            //マウスドラッグ終了
+            else if (InputProvider.Instance.GetFire1Up())
+            {
+                mouseDrag = false;
+            }
+
+            //マウスドラッグ中
+            //マウスの動きと反対方向に発射される
+            if (mouseDrag)
+            {
+                //攻撃
+                if (Input.GetKeyDown(KeyCode.A) && Action)
+                {
+                    //マウスのドラッグ終了ポジションを代入
+                    Vector2 endPos = Input.mousePosition;
+
+                    //ドラッグ開始ポジションと終了ポジション間の角度を計算
+                    Test_Koma1.attackRad = Mathf.Repeat(Mathf.Atan2(startPos.y - endPos.y, startPos.x - endPos.x) * Mathf.Rad2Deg, 360);
+
+                    //攻撃オブジェクトの生成位置を計算
+                    Vector2 objPos = comaObject.transform.position;
+                    objPos.x += attackPrefab.transform.localScale.x / 2f * Mathf.Cos(Test_Koma1.attackRad * Mathf.Deg2Rad);
+                    objPos.y += attackPrefab.transform.localScale.x / 2f * Mathf.Sin(Test_Koma1.attackRad * Mathf.Deg2Rad);
+
+                    //攻撃プレハブを元に、オブジェクトを生成、
+                    Test_Koma1.attackObj = Instantiate(attackPrefab, objPos, Quaternion.Euler(0.0f, 0.0f, Test_Koma1.attackRad));
+
+                    Test_Koma1.attackStartTime = time;
+                    Action = false;
+                }
+                //回避
+                else if (Input.GetKeyDown(KeyCode.Space) && Avoidance)
+                {
+                    Debug.Log("回避");
+                    //マウスのドラッグ終了ポジションを代入
+                    Vector2 endPos = Input.mousePosition;
+
+                    //(ドラッグ終了ポジション - ドラッグ開始ポジション)に-１を掛けている
+                    KomaMove(endPos);
+
+                    Avoidance = false;
+                }
+            }
+        }
+    }
+
+    //これは、値を返さない変数
+    void KomaMove(Vector2 endPos)
+    {
+        //(離した座標 - 押した座標)に-１を掛けている
+        Vector2 startDirection = -1 * (endPos - startPos).normalized;
+        this.rigid2D.AddForce(startDirection * speed);
     }
 }
--- Assets/Users/Yuta/Script/Test_Player2.cs
+++ Assets/Users/Yuta/Script/Test_Player2.cs
@@ -4,15 +4,146 @@ using UnityEngine;
 
 public class Test_Player2 : MonoBehaviour
 {
+    //コントローラー操作
+
+    Rigidbody2D rigid2D;
+    Vector2 startPos;
+    [SerializeField] private GameObject attackPrefab;
+    [SerializeField] private GameObject comaObject;
+
+    private float speed;
+    public static float start_time = 0f;
+    public static float time = 0f;
+    private float Action_time = 1f;
+
+    public static bool start = true;
+    public static bool Action = false;
+    public static bool Avoidance = false;
+    public static bool Ready_P2 = false;
+    public static bool Start_Time_P2 = false;
+
+    private float h;
+    private float v;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.rigid2D = comaObject.GetComponent<Rigidbody2D>();
+        start = true;
+        Action = false;
+        Avoidance = false;
+        Ready_P2 = false;
+
+        start_time = 0f;
+        time = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        h = Input.GetAxis("Horizontal_Test");
+        v = Input.GetAxis("Vertical_Test");
+
+        if (start)
+        {
+            this.startPos = new Vector2(0, 0);
+
+            if (Start_Time_P2 && Test_Player1.Start_Time_P1)
+            {
+                Debug.Log("レディー");
+                start_time += Time.deltaTime;
+            }
+
+            if (h != 0 || v != 0)
+            {
+                Start_Time_P2 = true;
+            }
+
+            if (start_time >= 3f)
+            {
+                if (h != 0 || v != 0)
+                {
+                    //スティックのポジションを代入
+                    Vector2 endPos = new Vector2(h, v);
+                    //スティックの動きと反対方向に移動
+                    KomaMove(endPos);
+
+                    start = false;
+                    Action = true;
+                    Avoidance = true;
+                }
+            }
+        }
+        else
+        {
+            time += Time.deltaTime;
+            Ready_P2 = false;
+            Start_Time_P2 = false;
+        }
+
+        //攻撃＆回避
+        if (Action_time <= time)
+        {
+            this.startPos = new Vector2(0, 0);
+
+            //攻撃
+            if (Input.GetButtonDown("Attack_Test") && Action)
+            {
+                //マウスのドラッグ終了ポジションを代入
+                Vector2 endPos = new Vector2(h, v);
+
+                //ドラッグ開始ポジションと終了ポジション間の角度を計算
+                Test_Koma2.attackRad = Mathf.Repeat(Mathf.Atan2(startPos.y - endPos.y, startPos.x - endPos.x) * Mathf.Rad2Deg, 360);
+
+                //攻撃オブジェクトの生成位置を計算
+                Vector2 objPos = comaObject.transform.position;
+                objPos.x += attackPrefab.transform.localScale.x / 2f * Mathf.Cos(Test_Koma2.attackRad * Mathf.Deg2Rad);
+                objPos.y += attackPrefab.transform.localScale.x / 2f * Mathf.Sin(Test_Koma2.attackRad * Mathf.Deg2Rad);
+
+                //攻撃プレハブを元に、オブジェクトを生成、
+                Test_Koma2.attackObj = Instantiate(attackPrefab, objPos, Quaternion.Euler(0.0f, 0.0f, Test_Koma2.attackRad));
+
+                Test_Koma2.attackStartTime = time;
+                Action = false;
+            }
+            //回避
+            if (Input.GetButtonDown("Avoidance_Test") && Avoidance)
+            {
+                //マウスのドラッグ終了ポジションを代入
+                Vector2 endPos = new Vector2(h, v);
+
+                KomaMove(endPos);
+
+                Avoidance = false;
+            }
+        }
+    }
+
+    //これは、値を返さない変数
+    void KomaMove(Vector2 endPos)
+    {
+        //(離した座標 - 押した座標)に-１を掛けている
+        Vector2 startDirection = -1 * (endPos - startPos).normalized;
+
+        //vとhを２乗とルートで計算
+        float d = Mathf.Sqrt(Mathf.Pow(h, 2) + Mathf.Pow(v, 2));
+
+        if (d >= 0f && d <= 0.25f)
+        {
+            speed = 25000f;
+        }
+        if (d >= 0.25f && d <= 0.5f)
+        {
+            speed = 50000f;
+        }
+        if (d >= 0.5f && d <= 0.75f)
+        {
+            speed = 75000f;
+        }
+        if (d >= 0.75f && d <= 1f)
+        {
+            speed = 100000f;
+        }
+        this.rigid2D.AddForce(startDirection * speed);
     }
 }
